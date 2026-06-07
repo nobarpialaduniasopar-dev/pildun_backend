a42d10547fed5669cb6e3e0b632e0c0e45b23fbb
TicketGo <ticketgo.ticketing.id@gmail.com>
2026-06-07 23:22:24 +0700

    homepage hotmatch and match fix

--- app/Models/AppSetting.php
+++ app/Models/AppSetting.php
@@ -2,13 +2,20 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class AppSetting extends Model
 {
+    use HasFactory;
+
+    // MATIKAN ASUMSI DEFAULT LARAVEL
     protected $primaryKey = 'key';
     public $incrementing = false;
     protected $keyType = 'string';
-    
-    protected $guarded = [];
+
+    protected $fillable = [
+        'key',
+        'value',
+    ];
 }
